027248d823d6dc1ed45dc78c005c6ecb9bd3a237
munibk22 <munib22@gmail.com>
2023-04-10 01:32:26 -0400

    updated Dalle routes

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY2,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
@@ -21,12 +21,13 @@ router.route('/').post(async (req, res) => {
     const { prompt } = req.body;
 
     const response = await openai.createImage({
-      prompt:"create small kitten image",
+      prompt,
       n: 1,
-      size: "1024x1024",
+      size: '1024x1024',
+      response_format: 'b64_json'
     });
 
-    const image = response.data.data[0].url;
+    const image = response.data.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
